6e0d42b15234b217ba1e3b9c4ca659db4153af9f
Jhon Livias <jhonlivias3@gmail.com>
2025-04-18 15:44:42 -0500

    update image delete route

--- app/Image/Routes/api.php
+++ app/Image/Routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(ImageController::class)->group(function() {
     Route::post('/images/upload', 'upload');
     Route::post('/images/multiple-upload', 'multipleUpload');
-    Route::delete('/images/{path}', 'delete');
+    Route::delete('/images/{path}', 'delete')->where('path', '.*');;
     Route::get('/images', 'getAll');
     Route::get('/images/{image}', 'get');
 });
